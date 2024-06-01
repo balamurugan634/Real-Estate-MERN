db0545980893b9db35c9e9dbc01d46ed801e34dd
balamurugan634 <bala060304@gmail.com>
2024-06-01 19:45:59 +0530

    create the test route api

---
+++ api/controllers/user.control.js
@@ -0,0 +1,6 @@
+const test=(req,res)=>{
+    res.json({
+        message:"test success"
+    })
+}
+export default test
--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import UserRouter from './routes/user.route.js'
 dotenv.config({path:'./.env'})
 const app=express()
 mongoose.connect(process.env.MONGO).then(()=>console.log("connected")).catch((err)=>console.log(err))
 app.listen(3000,()=>{
     console.log("i am listening")
 })
+app.use('/api/user',UserRouter)
