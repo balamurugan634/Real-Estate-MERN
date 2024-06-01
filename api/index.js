c3a146f0360aaff85e8e0b96bdd16118188594cf
balamurugan634 <bala060304@gmail.com>
2024-06-01 16:06:26 +0530

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config({path:'./.env'})
 const app=express()
+mongoose.connect(process.env.MONGO).then(()=>console.log("connected")).catch((err)=>console.log(err))
 app.listen(3000,()=>{
     console.log("i am listening")
 })
