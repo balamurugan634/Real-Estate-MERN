9e0ffa4779617650a3aea8cbf96bd0dad7931b02
balamurugan634 <bala060304@gmail.com>
2024-06-23 13:34:31 +0530

    feat:create list functionality done

--- client/src/pages/Createlist.jsx
+++ client/src/pages/Createlist.jsx
@@ -6,36 +6,53 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
+import { set } from "mongoose";
 const Createlist = () => {
   const [files, Setfiles] = useState([]);
   console.log(files);
   const [formdata, setformdata] = useState({
     imageUrl: [],
+    name: "",
+    description: "",
+    address: "",
+    regularprice: 4,
+    discountprice: 3,
+    bathrooms: 2,
+    bedrooms: 2,
+    furnished: false,
+    parking: false,
+    offers: true,
+    type: "rent",
+    useRef: "hhh",
   });
   const [uploadprog, setuploadprog] = useState(false);
   const [uperror, setuperror] = useState(false);
-  console.log(formdata);
+  const [subload, setsubload] = useState(false);
+  const [suberror, setsuberror] = useState(false);
+  // console.log(formdata);
   function handleUpload(e) {
     if (files.length > 0 && files.length + formdata.imageUrl.length < 7) {
-      setuploadprog(true)
+      setuploadprog(true);
       const promises = [];
       for (let i = 0; i < files.length; i++) {
         promises.push(storeImage(files[i]));
       }
-      Promise.all(promises).then((urls) => {
-        setformdata({ ...formdata, imageUrl: formdata.imageUrl.concat(urls) });
-        setuperror(false);
-        setuploadprog(false)
-      }).catch((err)=>{
-        setuperror('max of 3mb')
-        setuploadprog(false)
-
-      })
-    }
-    else{
-      setuperror('max 6 images only')
-      setuploadprog(false)
-
+      Promise.all(promises)
+        .then((urls) => {
+          setformdata({
+            ...formdata,
+            imageUrl: formdata.imageUrl.concat(urls),
+          });
+          setuperror(false);
+          setuploadprog(false);
+        })
+        .catch((err) => {
+          setuperror("max of 3mb");
+          setuploadprog(false);
+        });
+    } else {
+      setuperror("max 6 images only");
+      setuploadprog(false);
     }
   }
   const storeImage = async (file) => {
@@ -62,16 +79,58 @@ const Createlist = () => {
       );
     });
   };
-  function handleDel(index){
-    setformdata({...formdata,imageUrl:formdata.imageUrl.filter((_,i)=>i !== index)})
+  function handleDel(index) {
+    setformdata({
+      ...formdata,
+      imageUrl: formdata.imageUrl.filter((_, i) => i !== index),
+    });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      setsuberror(false)
+      setsubload(true)
+      const res=await fetch('/api/user/createlisting/',{
+        method:'POST',
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(formdata)
+
+      })
+      const data=res.json()
+      console.log(data)
+      if(data.success==='false'){
+          setsuberror(data.message)
+          setsubload(false)
+      }
+      setsuberror(false)
+      setsubload(false)
+    } catch (error) {
+      setsuberror(error.message)
+      setsubload(false)
+    }
+    
+  }
+  function handleChange(e){
+    if (e.target.id==='sale' || e.target.id==='rent'){
+      setformdata({...formdata,type:e.target.id})
+    }
+    if(e.target.id==='furnished' || e.target.id==='parking' || e.target.id==='offers'){
+      setformdata({...formdata,[e.target.id]:e.target.checked})
+    }
+    if(e.target.type==='text' || e.target.type==='number' || e.target.type==='textarea'){
+      setformdata({...formdata,[e.target.id]:e.target.value})
+    }
 
   }
+  
   return (
     <main className="max-w-6xl p-2 mx-auto">
       <h1 className="text-center font-semibold text-3xl my-5 uppercase">
         Create List
       </h1>
-      <form action="" className="flex gap-4 flex-col sm:flex-row">
+      <form action="" onSubmit={handleSubmit} className="flex gap-4 flex-col sm:flex-row">
         <div className="flex flex-col p-2 gap-2 flex-1">
           <input
             type="text"
@@ -81,12 +140,16 @@ const Createlist = () => {
             minLength="10"
             placeholder="name"
             id="name"
+            onChange={handleChange}
+            value={formdata.name}
           />
           <textarea
             className="p-3 rounded-lg bg-slate-50"
             required
             placeholder="description"
-            id="discription"
+            id="description"
+            onChange={handleChange}
+            value={formdata.description}
           />
           <input
             type="text"
@@ -96,6 +159,8 @@ const Createlist = () => {
             minLength="10"
             placeholder="address"
             id="address"
+            onChange={handleChange}
+            value={formdata.address}
           />
           <div className="flex flex-wrap gap-6 p-2">
             <div className="flex gap-2">
@@ -104,6 +169,8 @@ const Createlist = () => {
                 className="ronded-lg border-gray-100 w-5"
                 name=""
                 id="sale"
+                onChange={handleChange}
+                checked={formdata.type === "sale"}
               />
               <span>Sell</span>
             </div>
@@ -113,6 +180,8 @@ const Createlist = () => {
                 className="ronded-lg border-gray-100 w-5"
                 name=""
                 id="rent"
+                onChange={handleChange}
+                checked={formdata.type === "rent"}
               />
               <span>Rent</span>
             </div>
@@ -122,6 +191,8 @@ const Createlist = () => {
                 className="ronded-lg border-gray-100 w-5"
                 name=""
                 id="parking"
+                onChange={handleChange}
+                checked={formdata.parking}
               />
               <span>Parking spot</span>
             </div>
@@ -131,6 +202,8 @@ const Createlist = () => {
                 className="ronded-lg border-gray-100 w-5"
                 name=""
                 id="furnished"
+                onChange={handleChange}
+                checked={formdata.furnished}
               />
               <span>Furnished</span>
             </div>
@@ -140,7 +213,9 @@ const Createlist = () => {
                 type="checkbox"
                 className="ronded-lg border-gray-100 w-5"
                 name=""
-                id="offer"
+                id="offers"
+                onChange={handleChange}
+                checked={formdata.offers}
               />
               <span>Offer</span>
             </div>
@@ -154,6 +229,8 @@ const Createlist = () => {
                 id="bedrooms"
                 min="1"
                 max="10"
+                onChange={handleChange}
+                value={formdata.bedrooms}
               />
               <p>Beds</p>
             </div>
@@ -165,6 +242,8 @@ const Createlist = () => {
                 id="bathrooms"
                 min="1"
                 max="10"
+                onChange={handleChange}
+                value={formdata.bathrooms}
               />
               <p>Bath</p>
             </div>
@@ -176,7 +255,9 @@ const Createlist = () => {
                 className="p-3 rounded-lg border border-gray-300"
                 id="regularprice"
                 min="1"
-                max="10"
+                max="10000000"
+                onChange={handleChange}
+                value={formdata.regularprice}
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
@@ -190,7 +271,9 @@ const Createlist = () => {
                 className="p-3 rounded-lg border border-gray-300"
                 id="discountprice"
                 min="1"
-                max="10"
+                max="10000000"
+                onChange={handleChange}
+                value={formdata.discountprice}
               />
               <div className="flex flex-col items-center">
                 <p>Discount price</p>
@@ -203,7 +286,6 @@ const Createlist = () => {
           <p className="font-semibold">
             Images :
             <span className="font-normal text-gray-600 ">
-           
               first image will be the cover (max 6)
             </span>
           </p>
@@ -222,24 +304,33 @@ const Createlist = () => {
               disabled={uploadprog}
               className="p-3 rounded-lg  border-green-600 text-green-600 uppercase border   "
             >
-              {uploadprog ? 'loading...' : 'upload'}
+              {uploadprog ? "loading..." : "upload"}
             </button>
           </div>
-          <button className=" font-semibold text-white hover:opacity-95 p-3 rounded-lg uppercase bg-blue-600">
-            create
-          </button>
+          
           <p className="text-red-600 text-center">{uperror && uperror}</p>
-        {formdata.imageUrl.length > 0 && formdata.imageUrl.map((url,index)=>
-          // console.log(url)
-          <div className="flex justify-between gap-2" key={url}>
-            <img src={url} alt="listimg" className="object-contain w-20 h-20 rounded-lg"/>
-            <button  type="button" onClick={()=>handleDel(index)} className="text-red-700 uppercase">delete</button>
-          </div>
-
-        )
-
-        }
-        
+          {formdata.imageUrl.length > 0 &&
+            formdata.imageUrl.map((url, index) => (
+              // console.log(url)
+              <div className="flex justify-between gap-2" key={url}>
+                <img
+                  src={url}
+                  alt="listimg"
+                  className="object-contain w-20 h-20 rounded-lg"
+                />
+                <button
+                  type="button"
+                  onClick={() => handleDel(index)}
+                  className="text-red-700 uppercase"
+                >
+                  delete
+                </button>
+              </div>
+            ))}
+            <button type='submit' disabled={subload} className=" font-semibold text-white hover:opacity-95 p-3 rounded-lg uppercase bg-blue-600 disabled:bg-gray-400">
+            {subload ? 'loading...' : 'create' }
+          </button>
+          <p className="text-red-600 text-center">{suberror && suberror}</p>
         </div>
       </form>
     </main>
