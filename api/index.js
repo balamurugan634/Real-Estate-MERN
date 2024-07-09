2c327690b51c05cec8d351c92f6e68aea564c600
balamurugan634 <bala060304@gmail.com>
2024-07-09 16:56:38 +0530

    deploy procedures

--- api/index.js
+++ api/index.js
@@ -5,7 +5,10 @@ import UserRouter from './routes/user.signup.route.js'
 import updaterouter from './routes/user.route.js'
 import cookieParser from 'cookie-parser'
 import ListingRouter from './routes/listing.route.js'
+import path from 'path'
 dotenv.config({path:'./.env'})
+const __dirname=path.resolve()
+
 const app=express()
 mongoose.connect(process.env.MONGO).then(()=>console.log("connected")).catch((err)=>console.log(err))
 app.listen(3000,()=>{
@@ -16,6 +19,10 @@ app.use(cookieParser())
 app.use('/api/user',updaterouter)
 app.use('/api/auth',UserRouter)
 app.use('/api/user',ListingRouter)
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 // app.use('/api/auth',loginrouter)
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500
